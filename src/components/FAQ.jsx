b35c7bbc600fcf65ffa7338525a3d16547dc5420
RoxZkiL <mttrox@gmail.com>
2024-07-06 17:11:14 -0400

    Faq and main updates

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,20 +1,22 @@
 import './FAQ.css';
+import img from "../assets/faqimg.jpg"
 
 export const FAQ = () => {
   return (
-    <section className="faq">
-      <div>
-        <h2>¿Qué es la ODS9?</h2>
-        <p>Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.</p>
+    <div className="faq">
+      <div className='img-div'>
+        <img src={img} alt='image not found'/>
       </div>
-      <div>
-        <h3>Metas</h3>
-        <p>Descripción de las metas.</p>
+      <div className='text-div'>
+      <div className='heading-div'>
+        <h2>¿Qué busca la ODS9?</h2>
+        <legend>Desarrollar infraestructuras robustas e impulsar una industrialización que respete el medio ambiente.</legend>
       </div>
-      <div>
-        <h3>Submetas</h3>
-        <p>Descripción de las submetas.</p>
+      <div className='main-text'>
+        <p>El Objetivo 9 pretende construir infraestructuras resilientes, promover la industrialización sostenible y fomentar la innovación.</p>
+        <p>El crecimiento económico, el desarrollo social y la acción por el clima dependen en gran medida de las inversiones en infraestructuras, el desarrollo industrial sostenible y el progreso tecnológico. Ante la rápida evolución del panorama económico mundial y el aumento de las desigualdades, el crecimiento sostenido debe implicar una industrialización que, en primer lugar, haga accesibles las oportunidades a todas las personas y, en segundo lugar, se apoye en la innovación y en infraestructuras resistentes.</p>
       </div>
-    </section>
+      </div>
+    </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,15 @@
 import './Main.css';
+import transparentVideo from '../assets/video.mp4'; // Asegúrate de tener el video en la ruta correcta
 
 const Main = () => {
   return (
     <main className="main">
+      <div className="video-container">
+        <video className="background-video" autoPlay loop muted playsInline>
+          <source src={transparentVideo} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <section className="intro">
         <h1>ODS 9: Desarrollo sostenible</h1>
         <p>Industria, innovación e infraestructura</p>

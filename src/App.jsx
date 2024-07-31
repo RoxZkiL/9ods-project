f4d6dd76bbe81866ab9731b0922e6c6275345335
RoxZkiL <mttrox@gmail.com>
2024-07-31 17:49:14 -0400

    Router implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 

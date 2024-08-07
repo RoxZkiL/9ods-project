f3771b533c6ebc280767c613cddc69caf3796137
RoxZkiL <mttrox@gmail.com>
2024-08-07 12:54:28 -0400

    x

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Contacto from "./components/Contacto/Contacto";
+import Contact from "./components/Contact/Contact";
 import Importance from "./components/Importance/Importance";
+import Register from "./components/Forms/RegisterForm/Register";
+import Login from "./components/Forms/LoginForm/Login";
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
         <Route path="/" element={<Navigate to="/inicio" />} />
         <Route path="/inicio" element={<LandingPage />} />
         <Route path="/importancia" element={<Importance />} />
-        <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contacto" element={<Contact />} />
       </Routes>
     </Router>
   );

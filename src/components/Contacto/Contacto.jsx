4dafc2b24ff24d109c509047e67037525cdffe07
RoxZkiL <mttrox@gmail.com>
2024-08-06 20:57:29 -0400

    deleted unsued vars

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Header from '../Layout/Header';
-import Footer from '../Layout/Footer';
-import Formulario from './Formulario';
+import Header from "../Layout/Header";
+import Footer from "../Layout/Footer";
+import Formulario from "./Formulario";
 
 const Contacto = () => {
   return (
     <>
-    <Header/>
-    <Formulario/>
-    <Footer/>
+      <Header />
+      <Formulario />
+      <Footer />
     </>
   );
-}
+};
 
 export default Contacto;

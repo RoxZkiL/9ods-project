ef954ff3d1541b496abd866bfceeba9ad2f25837
RoxZkiL <mttrox@gmail.com>
2024-08-08 20:48:32 -0400

    finished blog-section, responsive styles and pagination

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,17 @@
+import BlogSection from "./BlogSection";
 import SearchBar from "./SearchBar";
 import TitleImageBlog from "./TitleImageBlog";
+import Header from "../Layout/Header";
+import Footer from "../Layout/Footer";
 
 const Blog = () => {
   return (
     <>
+      <Header />
       <TitleImageBlog />
       <SearchBar />
+      <BlogSection />
+      <Footer />
     </>
   );
 };

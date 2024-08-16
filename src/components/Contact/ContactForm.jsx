13473442dc0fa8d2254f8547505f519e9de9681e
RoxZkiL <mttrox@gmail.com>
2024-08-16 12:50:42 -0400

    console.log deleted on coctacform

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -51,10 +51,6 @@ const ContactForm = () => {
     };
 
     try {
-      console.log("Service ID:", import.meta.env.VITE_EMAILJS_SERVICE_ID);
-      console.log("Template ID:", import.meta.env.VITE_EMAILJS_TEMPLATE_ID);
-      console.log("User ID:", import.meta.env.VITE_EMAILJS_USER_ID);
-
       const result = await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
